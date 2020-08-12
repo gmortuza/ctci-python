f5900d1408c00ca1b227a53a1bf9f393abdad9ca
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-08-11 22:35:13 -0600

    1_remove_linked_list done

--- 2_linked_list/1_remove_dups.py
+++ 2_linked_list/1_remove_dups.py
@@ -1,14 +1,36 @@
 from LinkedList import LinkedList
 
 
+def remove_dups(ll):
+    curr = ll.head
+    seen = set([curr.val])
+    while curr.next:
+        if curr.next.val in seen:
+            curr.next = curr.next.next
+        else:
+            seen.add(curr.next.val)
+            curr = curr.next
 
+def remove_dups_followup(ll):
+    # run time --> O(n^2)
+    # Space --> O(1)
+    current = ll.head
+    while current:
+        runner = current
+        while runner.next:
+            if runner.next.val == current.val:
+                # remove this
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
 
 
 ll = LinkedList()
-ll.generate(100, 0, 9)
-print(ll)
-remove_dups(ll)
-print(ll)
+#ll.generate(100, 0, 9)
+#print(ll)
+#remove_dups(ll)
+#print(ll)
 
 ll.generate(100, 0, 9)
 print(ll)
