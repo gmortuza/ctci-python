3672cc213dc140937a873674e20216a7bc6151ad
Golam Md Mortuza <golammdmortuza@u.boisestate.edu>
2020-08-21 17:05:37 -0600

    multistack done

--- 3_stack_heaps/1_multistack.py
+++ 3_stack_heaps/1_multistack.py
@@ -5,13 +5,34 @@ class MultiStack:
         self.array = [None] * (stack_capacity * self.num_stack)
         self.stack_sizes = [0] * self.num_stack
 
-    def push(self, stack_number):
+    def push(self, stack_number, value):
         """
         Push value in the stack
         :param stack_number: put value in this stack number
+        :param value: Value to be pushed
         :return:
         """
         if self.stack_sizes[stack_number] > self.stack_capacity:
             raise Exception("Stack full")
+        self.array[(stack_number*self.stack_capacity) + self.stack_sizes[stack_number]] = value
         self.stack_sizes[stack_number] += 1
-        self.array[stack_number*self.stack_capacity]
+
+    def pop(self, stack_number):
+        """
+
+        :return:
+        """
+        value = self.array[(stack_number * self.stack_capacity) + self.stack_sizes[stack_number] - 1]
+        self.array[(stack_number * self.stack_capacity) + self.stack_sizes[stack_number] - 1] = None
+        self.stack_sizes[stack_number] -= 1
+        return value
+
+
+if __name__ == '__main__':
+   stack = MultiStack(10)
+   stack.push(0, 1)
+   stack.push(1, 5)
+   stack.push(2, 4)
+   print(stack.pop(2))
+   print(stack.array)
+
